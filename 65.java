ba08401e5b51ab89674e4315d92d1b75f1e182b9
satyachidithoti <41900189+satyachidithoti@users.noreply.github.com>
2018-08-22 17:25:00 +0100

    Create 65.java

---
+++ 65.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		for(int i=0;i<1;System.out.print(str))
+		{
+			i++;
+		}
+ 
+	}
+}
