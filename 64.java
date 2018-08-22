739cdb2bf308f170f52d4ad3db9b261f0117a32e
satyachidithoti <41900189+satyachidithoti@users.noreply.github.com>
2018-08-22 17:22:18 +0100

    Create 64.java

---
+++ 64.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int m=s.nextInt();
+		if((m+n)%2==0)
+		System.out.print("even");
+		else
+		System.out.print("odd");
+	}
+}
+ 
